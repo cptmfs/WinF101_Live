9942ba269f28d88f05d1d49a42e8daa622ee8f52
Muhammed Ferit Simsek <cpt.mfs@gmail.com>
2023-03-20 13:45:34 +0300

    v_02

---
+++ MonthDays/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace MonthDays
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nudYear = new System.Windows.Forms.NumericUpDown();
+            this.cbxMonth = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnList = new System.Windows.Forms.Button();
+            this.lbxDays = new System.Windows.Forms.ListBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.nudYear)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // nudYear
+            // 
+            this.nudYear.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.nudYear.Location = new System.Drawing.Point(80, 44);
+            this.nudYear.Maximum = new decimal(new int[] {
+            2100,
+            0,
+            0,
+            0});
+            this.nudYear.Minimum = new decimal(new int[] {
+            2000,
+            0,
+            0,
+            0});
+            this.nudYear.Name = "nudYear";
+            this.nudYear.Size = new System.Drawing.Size(120, 24);
+            this.nudYear.TabIndex = 0;
+            this.nudYear.Value = new decimal(new int[] {
+            2000,
+            0,
+            0,
+            0});
+            // 
+            // cbxMonth
+            // 
+            this.cbxMonth.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.cbxMonth.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbxMonth.FormattingEnabled = true;
+            this.cbxMonth.Location = new System.Drawing.Point(80, 101);
+            this.cbxMonth.Name = "cbxMonth";
+            this.cbxMonth.Size = new System.Drawing.Size(121, 26);
+            this.cbxMonth.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Yıl";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 18);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Ay";
+            // 
+            // btnList
+            // 
+            this.btnList.Location = new System.Drawing.Point(80, 157);
+            this.btnList.Name = "btnList";
+            this.btnList.Size = new System.Drawing.Size(120, 26);
+            this.btnList.TabIndex = 4;
+            this.btnList.Text = "Listele";
+            this.btnList.UseVisualStyleBackColor = true;
+            // 
+            // lbxDays
+            // 
+            this.lbxDays.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lbxDays.FormattingEnabled = true;
+            this.lbxDays.ItemHeight = 18;
+            this.lbxDays.Location = new System.Drawing.Point(12, 189);
+            this.lbxDays.Name = "lbxDays";
+            this.lbxDays.Size = new System.Drawing.Size(377, 148);
+            this.lbxDays.TabIndex = 5;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(216, 157);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(120, 26);
+            this.btnExit.TabIndex = 6;
+            this.btnExit.Text = "Çıkış";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(401, 343);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.lbxDays);
+            this.Controls.Add(this.btnList);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbxMonth);
+            this.Controls.Add(this.nudYear);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nudYear)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown nudYear;
+        private System.Windows.Forms.ComboBox cbxMonth;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnList;
+        private System.Windows.Forms.ListBox lbxDays;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
