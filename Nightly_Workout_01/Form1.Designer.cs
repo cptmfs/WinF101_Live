71ff627dccd9a729e9d6004681b92d95196730c5
Muhammed Ferit Simsek <cpt.mfs@gmail.com>
2023-03-23 21:55:28 +0300

    v_WO

---
+++ Nightly_Workout_01/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace Nightly_Workout_01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.cbxShipperCN = new System.Windows.Forms.ComboBox();
+            this.cbxEmployeeSearch = new System.Windows.Forms.ComboBox();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnShipperSearch = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnEmployeeSearch = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(646, 352);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // cbxShipperCN
+            // 
+            this.cbxShipperCN.FormattingEnabled = true;
+            this.cbxShipperCN.Location = new System.Drawing.Point(689, 40);
+            this.cbxShipperCN.Name = "cbxShipperCN";
+            this.cbxShipperCN.Size = new System.Drawing.Size(160, 26);
+            this.cbxShipperCN.TabIndex = 1;
+            // 
+            // cbxEmployeeSearch
+            // 
+            this.cbxEmployeeSearch.FormattingEnabled = true;
+            this.cbxEmployeeSearch.Location = new System.Drawing.Point(689, 157);
+            this.cbxEmployeeSearch.Name = "cbxEmployeeSearch";
+            this.cbxEmployeeSearch.Size = new System.Drawing.Size(160, 26);
+            this.cbxEmployeeSearch.TabIndex = 2;
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(689, 271);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(160, 24);
+            this.txtSearch.TabIndex = 3;
+            this.txtSearch.TextChanged += new System.EventHandler(this.txtSearch_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(672, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(190, 18);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Shipper Company Name";
+            // 
+            // btnShipperSearch
+            // 
+            this.btnShipperSearch.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btnShipperSearch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnShipperSearch.Location = new System.Drawing.Point(719, 72);
+            this.btnShipperSearch.Name = "btnShipperSearch";
+            this.btnShipperSearch.Size = new System.Drawing.Size(100, 31);
+            this.btnShipperSearch.TabIndex = 5;
+            this.btnShipperSearch.Text = "Search";
+            this.btnShipperSearch.UseVisualStyleBackColor = false;
+            this.btnShipperSearch.Click += new System.EventHandler(this.btnShipperSearch_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(725, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 18);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Employee";
+            // 
+            // btnEmployeeSearch
+            // 
+            this.btnEmployeeSearch.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btnEmployeeSearch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEmployeeSearch.Location = new System.Drawing.Point(719, 189);
+            this.btnEmployeeSearch.Name = "btnEmployeeSearch";
+            this.btnEmployeeSearch.Size = new System.Drawing.Size(100, 31);
+            this.btnEmployeeSearch.TabIndex = 7;
+            this.btnEmployeeSearch.Text = "Search";
+            this.btnEmployeeSearch.UseVisualStyleBackColor = false;
+            this.btnEmployeeSearch.Click += new System.EventHandler(this.btnEmployeeSearch_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(702, 240);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(130, 18);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Find in DataGrid";
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExit.Location = new System.Drawing.Point(719, 321);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(100, 31);
+            this.btnExit.TabIndex = 9;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(874, 360);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnEmployeeSearch);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnShipperSearch);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.cbxEmployeeSearch);
+            this.Controls.Add(this.cbxShipperCN);
+            this.Controls.Add(this.dataGridView1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox cbxShipperCN;
+        private System.Windows.Forms.ComboBox cbxEmployeeSearch;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnShipperSearch;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnEmployeeSearch;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ Nightly_Workout_01/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Nightly_Workout_01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection connection = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Northwind;Integrated Security=True");
+        private void ListOrders()
+        {
+            SqlCommand cmd = new SqlCommand("Select * from OrderGrid", connection);
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+        private void PrepareGrid()
+        {
+            dataGridView1.AllowUserToAddRows = false; // Veri girişi yapılmasın
+            dataGridView1.AllowUserToDeleteRows = false; // Veri silinmesin
+            dataGridView1.RowHeadersVisible = false; // satırların solunda başlık olmasın
+            //dataGridView1.AutoGenerateColumns=false; // 
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill; // Tüm datagrid ekranını kaplasın tablolar ( 1 tablo olsa bile gri alan bırakmasın )
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect; // tek tek seçmek yerine tüm satırı seçme
+            dataGridView1.ReadOnly = true;// sadece okunabilsin..
+            dataGridView1.RowHeadersVisible=false;
+        }
+        private void LoadComboboxes()
+        {
+
+            SqlCommand cmd2 = new SqlCommand("Select * from Shippers", connection);
+            SqlDataAdapter adapter2= new SqlDataAdapter(cmd2);  
+            DataTable dt2 = new DataTable();
+            adapter2.Fill(dt2);
+            cbxShipperCN.DataSource= dt2;
+            cbxShipperCN.DisplayMember = "CompanyName";
+            cbxShipperCN.ValueMember = "ShipperID";
+
+            SqlCommand cmd4 = new SqlCommand("Select FirstName+' ' + LastName as 'Employee',EmployeeID from Employees",connection);
+            SqlDataAdapter adapter4 = new SqlDataAdapter(cmd4);
+            DataTable dt4 = new DataTable();
+            adapter4.Fill(dt4);
+            cbxEmployeeSearch.DataSource= dt4;
+            cbxEmployeeSearch.DisplayMember = "Employee";
+            cbxEmployeeSearch.ValueMember = "EmployeeID";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ListOrders();
+            LoadComboboxes();
+        }
+
+        private void btnShipperSearch_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd3 = new SqlCommand("Select * from OrderGrid where ShipperID="+cbxShipperCN.SelectedValue+"",connection);
+            SqlDataAdapter adapter3 = new SqlDataAdapter(cmd3);
+            DataTable dt3 = new DataTable();
+            adapter3.Fill(dt3);
+            dataGridView1.DataSource= dt3;
+        }
+
+        private void btnEmployeeSearch_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd4 = new SqlCommand("Select * from OrderGrid where EmployeeID=" + cbxEmployeeSearch.SelectedValue + "", connection);
+            SqlDataAdapter adapter4 =new SqlDataAdapter(cmd4);
+            DataTable dt4 = new DataTable();
+            adapter4.Fill(dt4);
+            dataGridView1.DataSource= dt4;
+        }
+
+        private void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+            SqlDataAdapter adapter5 = new SqlDataAdapter("Select * from OrderGrid where CustomerCompanyName like '%"+txtSearch.Text+ "%' or EmployeeNameSurname like '%" + txtSearch.Text+ "%' or ShipperCompanyName like '%"+txtSearch.Text+"%'", connection);
+            DataTable dt5 = new DataTable();    
+            adapter5.Fill(dt5); 
+            dataGridView1.DataSource= dt5;
+            if (txtSearch.Text=="")
+            {
+                ListOrders();
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
